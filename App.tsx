e750537ba305cd0d86be7c2a6631c8a0d1bcde8a
Christopher Verbsky <chrisverbsky@gmail.com>
2025-03-08 18:51:59 -0600

    Removed Expo framework.

--- App.tsx
+++ App.tsx
@@ -4,49 +4,96 @@ import * as React from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Index from './app/(tabs)/index'
 import NewPost from './app/(tabs)/new-post'
 import MyPosts from './app/(tabs)/my-posts'
 
 
-function HomeScreen() {
+function MyTabBar({ state, descriptors, navigation }) {
+  const { colors } = useTheme();
+  const { buildHref } = useLinkBuilder();
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
+    <View style={{ flexDirection: 'row' }}>
+      {state.routes.map((route, index) => {
+        const { options } = descriptors[route.key];
+        const label =
+          options.tabBarLabel = 'false'
+
+        const isFocused = state.index === index;
+
+        const onPress = () => {
+          const event = navigation.emit({
+            type: 'tabPress',
+            target: route.key,
+            canPreventDefault: true,
+          });
+
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name, route.params);
+          }
+        };
+
+        const onLongPress = () => {
+          navigation.emit({
+            type: 'tabLongPress',
+            target: route.key,
+          });
+        };
+
+        return (
+          <PlatformPressable
+            href={buildHref(route.name, route.params)}
+            accessibilityState={isFocused ? { selected: true } : {}}
+            accessibilityLabel={options.tabBarAccessibilityLabel}
+            testID={options.tabBarButtonTestID}
+            onPress={onPress}
+            onLongPress={onLongPress}
+            style={{ flex: 1 }}
+          >
+            <Text style={{ color: isFocused ? colors.primary : colors.text }}>
+              {label}
+            </Text>
+          </PlatformPressable>
+        );
+      })}
     </View>
   );
 }
 
-const Stack = createNativeStackNavigator();
 
-function RootStack() {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreen} />
-    </Stack.Navigator>
-  );
-}
+//const Stack = createNativeStackNavigator();
+const Tabs = createBottomTabNavigator({
+  screens: {
+    Home: Index,
+    NewPost: NewPost,
+    MyPosts: MyPosts,
+  }
+})
+
+
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
+      <Tabs.Navigator>
+        <Tabs.Screen
           name="Home"
           component={Index}
           options={{title: 'Welcome'}}
         />
-        <Stack.Screen
+        <Tabs.Screen
           name="New Post"
           component={NewPost}
           options={{title: 'Add a Gift'}}
         />
-        <Stack.Screen
+        <Tabs.Screen
           name="My Posts"
           component={MyPosts}
           options={{title: 'My Gifts'}}
         />
-      </Stack.Navigator>
+      </Tabs.Navigator>
     </NavigationContainer>
   );
 }
